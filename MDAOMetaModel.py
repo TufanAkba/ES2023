21053e82eafb615c1aa7090f3bb83e58ffd20cc6
TufanAkba <55416811+TufanAkba@users.noreply.github.com>
2023-05-07 17:55:51 +0300

    Add files via upload
    initial commit of the python files. Also, doe .csv s are uploaded.

---
+++ MDAOMetaModel.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  8 11:23:26 2022
+
+@author: tufanakba
+Meta model test for problem solving
+"""
+
+import numpy as np
+import openmdao.api as om
+import matplotlib.pyplot as plt
+plt.rcParams.update({'font.size': 16})
+# import seaborn as sns
+import pandas as pd
+import time
+
+if __name__ == "__main__":
+    
+
+    
+    st = time.time()
+    p = om.Problem()
+    
+    #get train data
+    folder = 'train50'
+    vol = np.loadtxt(folder + '/vol.csv')
+    Tfo = np.loadtxt(folder + '/Tfo.csv')
+    Tfi = np.loadtxt(folder + '/Tfi.csv')
+    T_o = np.loadtxt(folder + '/T_o.csv')
+    rpc = np.loadtxt(folder + '/rpc.csv')
+    m_d = np.loadtxt(folder + '/m_dot.csv')
+    L = np.loadtxt(folder + '/L.csv')
+    ins = np.loadtxt(folder + '/ins.csv')
+    d_nom = np.loadtxt(folder + '/d_nom.csv')
+    porosity = np.loadtxt(folder + '/porosity.csv')
+    
+    # surr = om.MetaModelUnStructuredComp(default_surrogate=om.ResponseSurface())
+    # surr = om.MetaModelUnStructuredComp(default_surrogate=om.NearestNeighbor(interpolant_type='linear'))
+    # surr = om.MetaModelUnStructuredComp(default_surrogate=om.NearestNeighbor(interpolant_type='rbf', num_neighbors=5, rbf_family=-2))
+    # surr = om.MetaModelUnStructuredComp(default_surrogate=om.NearestNeighbor(interpolant_type='weighted'))
+    surr = om.MetaModelUnStructuredComp(default_surrogate=om.KrigingSurrogate(eval_rmse= True))
+    
+    surr.add_input('Tfi',training_data = Tfi)
+    surr.add_input('m_dot',training_data = m_d)
+    surr.add_input('rpc',training_data=rpc)
+    surr.add_input('ins',training_data=ins)
+    surr.add_input('L',training_data=L)
+    surr.add_input('T_o',training_data=T_o)
+    surr.add_input('d_nom',training_data=d_nom)
+    surr.add_input('porosity',training_data=porosity)
+    # surr.add_input('vol', training_data=vol)
+    surr.add_output('Tfo', training_data=Tfo)
+    
+    p.model.add_subsystem('surr', surr, promotes=['*'])
+    p.setup()
+    
+    #get validation data
+    folder = 'valid'
+    vol = np.loadtxt(folder + '/vol.csv')
+    Tfo = np.loadtxt(folder + '/Tfo.csv')
+    Tfi = np.loadtxt(folder + '/Tfi.csv')
+    T_o = np.loadtxt(folder + '/T_o.csv')
+    rpc = np.loadtxt(folder + '/rpc.csv')
+    m_d = np.loadtxt(folder + '/m_dot.csv')
+    L = np.loadtxt(folder + '/L.csv')
+    ins = np.loadtxt(folder + '/ins.csv')
+    d_nom = np.loadtxt(folder + '/d_nom.csv')
+    porosity = np.loadtxt(folder + '/porosity.csv')
+    
+    # dataset = pd.read_csv('train100.csv')
+    # print(dataset.columns) # get column names
+    # inputs=dataset[['rpc','Tfi','m_dot','ins','L','d_nom','porosity']]
+    # outputs=dataset[['Tfo','vol','T_o']]
+    # outputs=dataset[['Tfo','T_o']]
+    
+    # sns.pairplot(dataset, diag_kind='kde')
+    # sns.pairplot(inputs, diag_kind='kde')
+    # sns.pairplot(outputs, diag_kind='kde')
+    
+    
+    err=[]
+    Tfo_surr = []
+
+    for i in range(Tfo.size):
+        # p.set_val('vol',vol[i])
+        p.set_val('Tfi',Tfi[i])
+        p.set_val('T_o',T_o[i])
+        p.set_val('rpc',rpc[i])
+        p.set_val('m_dot',m_d[i])
+        p.set_val('L',L[i])
+        p.set_val('ins',ins[i])
+        p.set_val('d_nom',d_nom[i])
+        p.set_val('porosity',porosity[i])
+        
+        p.final_setup()
+        p.run_model()
+        
+        Tfo_surr.append(float(p.get_val('Tfo')))
+
+
+    err=Tfo-Tfo_surr
+    
+    err=(1-Tfo_surr/Tfo)*100
+    
+    print("time", time.time() - st)
+    
+    plt.figure(figsize=(10, 8), dpi=100)
+    plt.hist(err, bins=10)
+    plt.xlabel('Prediction Error [%]')
+    _ = plt.ylabel('Count')
+    
+    min_lim = min(np.amin(Tfo_surr),np.amin(Tfo))
+    max_lim = max(np.amax(Tfo_surr),np.amax(Tfo))
+    
+
+    
+    plt.figure(figsize=(10, 8), dpi=100)
+    plt.axes(aspect='equal')
+    plt.scatter(Tfo, Tfo_surr)
+    plt.xlabel('True Values')
+    plt.ylabel('Predictions')
+    lims = [min_lim, max_lim]
+    plt.xlim(lims)
+    plt.ylim(lims)
+    
+    RMS_text = "{:.2%}".format((np.corrcoef(Tfo, Tfo_surr)[0,1])**2)
+    RMSE = np.sqrt(np.square(np.subtract(Tfo,Tfo_surr)).mean())
+    
+    props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+    plt.text(1.05*min_lim, 0.95*max_lim, r'$R^2$' + f' = {RMS_text}',bbox=props)
+    
+    _ = plt.plot(lims, lims)
+    
+    
